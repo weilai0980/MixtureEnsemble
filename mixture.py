97a7edb970c8877debfad56d16279c6f5f0005c2
weilai0980 <tian.guo0980@gmail.com>
2019-03-08 16:54:50 +0100

    linear

--- mixture.py
+++ mixture.py
@@ -18,20 +18,17 @@ import collections
 import hashlib
 import numbers
 
-from tensorflow.python.ops import rnn_cell_impl
-from tensorflow.python.ops.rnn_cell_impl import * 
-
 # library for bayesian inference
 import edward as ed
 from edward.models import Categorical, Dirichlet, Empirical, InverseGamma, MultivariateNormalDiag, Normal, ParamMixture, Beta, Bernoulli, Mixture
 
 # local packages
 from utils_libs import *
-from ts_mv_rnn_basics import *
+#from ts_mv_rnn_basics import *
 
 # reproducibility by fixing the random seed
-np.random.seed(1)
-tf.set_random_seed(1)
+# np.random.seed(1)
+# tf.set_random_seed(1)
 
 # ---- utilities functions ----
 
@@ -54,7 +51,7 @@ def linear(x,
             h = tf.matmul(x, w)
            
         #l2
-        regularizer = tf.nn.l2_loss(w)
+        regularizer = tf.reduce_sum(tf.square(w_v))
         
     return tf.squeeze(h), regularizer
 
@@ -64,7 +61,6 @@ def bilinear(x,
              bool_bias):
     
     # shape of x: [b, t, v]
-
     with tf.variable_scope(scope):
         
         w_l = tf.Variable(name = 'w_left', 
@@ -83,38 +79,79 @@ def bilinear(x,
         else:
             h = tf.matmul(tmph, w_l)
             
-    # l2, l1 regularization
-    #tf.nn.l2_loss(w_t) tf.reduce_sum(tf.abs(w_v)
-    return tf.squeeze(h), tf.reduce_sum(tf.square(w_t)) + tf.reduce_sum(tf.abs(w_v)) 
+    return tf.squeeze(h), tf.reduce_sum(tf.square(w_t)) + tf.reduce_sum(tf.square(w_v)) 
 
 # ---- Mixture linear ----
 
 class mixture_linear():
+    
+    def __init__(self, session, loss_type):
+        
+        # build the network graph 
+        self.lr = 0.0
+        self.l2 = 0.0
+        
+        self.epsilon = 1e-3
+        
+        self.sess = session
+        
+        self.bool_log = ''
+        self.loss_type = loss_type
+        self.distr_type = ''
+        
 
-    def __init__(self, 
+    def network_ini(self, 
                  session, 
                  lr, 
                  l2, 
-                 batch_size, 
                  dim_x_list,
                  steps_x_list, 
                  bool_log, 
                  bool_bilinear,
                  loss_type, 
                  distr_type, 
-                 activation_type, 
-                 pos_regu, 
-                 gate_type):
+                 bool_regu_positive_mean,
+                 bool_regu_gate):
+        
+        '''
+        Args:
+        
+        session: tensorflow session
+        
+        lr: float, learning rate
+        
+        l2: float, l2 regularization
+        
+        dim_x_list: list of dimension values for each component in X
+        
+        steps_x_list: list of sequence length values for each component in X
+        
+        bool_log: if log operation is on the targe variable Y
+        
+        bool_bilinear: if bilinear function is used on the components in X
+        
+        loss_type: string, type of loss functions, {mse, lk, lk_inv}
+        
+        distr_type: string, type of the distribution of the target variable
+        
+        bool_regu_positive_mean: if regularization of positive mean 
+        
+        bool_regu_gate: if regularization the gate functions
+        
+        '''
         
         # bool_hidden_depen
         
-        # build the network graph 
-        self.LEARNING_RATE = lr
-                
-        self.N_BATCH = batch_size
-        self.L2 = l2
+        # ---- fix the random seed to reproduce the results
+        np.random.seed(1)
+        tf.set_random_seed(1)
         
-        self.MAX_NORM = 0.0
+        # ---- ini
+        
+        # build the network graph 
+        self.lr = lr
+        self.l2 = l2
+        
         self.epsilon = 1e-3
         
         self.sess = session
@@ -139,7 +176,7 @@ class mixture_linear():
                 x_ph_list.append(tf.placeholder(tf.float32, [None, steps_x_list[i]*dim_x_list[i]], name = 'x' + str(i)))
                 
                 
-        # --- prediction of individual models
+        # ---- prediction of individual models
         
         mean_list = []
         var_list = []
@@ -153,9 +190,10 @@ class mixture_linear():
             
             if bool_bilinear == True:
                 
+                #[B]
                 tmp_mean, tmp_regu_mean = bilinear(x_ph_list[i], 
                                                    [steps_x_list[i], dim_x_list[i]], 
-                                                   'mean' + str(i) , 
+                                                   'mean' + str(i), 
                                                    True)
                 
                 tmp_var, tmp_regu_var = bilinear(x_ph_list[i], 
@@ -168,9 +206,9 @@ class mixture_linear():
                                                     'gate' + str(i),
                                                     True)
                 
-                
             else:
                 
+                #[B]
                 tmp_mean, tmp_regu_mean = linear(x_ph_list[i], 
                                                  dim_x_list[i], 
                                                  'mean' + str(i),
@@ -183,7 +221,7 @@ class mixture_linear():
                 
                 tmp_logit, tmp_regu_gate = linear(x_ph_list[i], 
                                                   dim_x_list[i], 
-                                                  'var' + str(i), 
+                                                  'gate' + str(i), 
                                                   True)
                 
             # [C B 1]
@@ -199,30 +237,13 @@ class mixture_linear():
         # [B C]
         mean_stack = tf.stack(mean_list, 1)
         var_stack = tf.stack(var_list, 1)
+        inv_var_stack = tf.stack(var_list, 1)
             
         # [B C]
         self.logit = tf.stack(logit_list, 1)
         self.gates = tf.nn.softmax(self.logit, axis = -1)
         
-        
-        # --- negative log likelihood 
-        
-        # nllk: negative log likelihood
-            
-        #[B C]
-        tmpllk_indi_hetero = tf.exp(-0.5*tf.square(self.y - mean_stack)/(var_stack + 1e-5))/(2.0*np.pi*(var_stack + 1e-5))**0.5
-            
-        llk_hetero = tf.multiply(tmpllk_indi, self.gates) 
-        self.nllk_hetero = tf.reduce_sum(-1.0*tf.log(tf.reduce_sum(llk, axis = -1) + 1e-5))
-        
-        #[B C]
-        tmpllk_indi_const = tf.exp(-0.5*tf.square(self.y - mean_stack))/(2.0*np.pi)**0.5
-            
-        llk_const = tf.multiply(tmpllk_indi, self.gates) 
-        self.nllk_const = tf.reduce_sum(-1.0*tf.log(tf.reduce_sum(llk, axis = -1) + 1e-5))
-        
-        
-        # --- regularization
+        # ---- regularization
         
         # temporal coherence, diversity 
         # gate smoothness 
@@ -231,51 +252,101 @@ class mixture_linear():
         # mean diversity
         
         # mean non-negative  
-        regu_mean_pos = tf.reduce_sum( tf.maximum(0.0, -1.0*mean_v) + tf.maximum(0.0, -1.0*mean_x) )
+        # regu_mean_pos = tf.reduce_sum( tf.maximum(0.0, -1.0*mean_v) + tf.maximum(0.0, -1.0*mean_x) )
         
-        self.regularization = 0.0
+        self.regularization = l2*regu_mean
         
+        '''
         if loss_type == 'sq' and distr_type == 'gaussian':
-            self.regularization = l2 * (regu_mean)
+            self.regularization = l2*(regu_mean)
                 
         elif loss_type == 'lk' and distr_type == 'gaussian':
-            self.regularization = l2 * (regu_mean + regu_var)
+            self.regularization = l2*(regu_mean + regu_var)
                 
         else:
             print '[ERROR] loss type'
+        '''
         
         # activation and hinge regularization 
-        if para_regu_positive_mean == True:
-            self.regularization += l2*regu_mean_pos
+        if bool_regu_positive_mean == True:
+            self.regularization += 0.1*l2*regu_mean_pos
             
-        if para_regu_gate == True:
+        if bool_regu_gate == True:
             self.regularization += 0.1*l2*regu_gate
+            
+            
+        # ---- negative log likelihood 
         
-        # --- prediction 
+        # nllk: negative log likelihood
+            
+        # lk    
+        # [B C]
+        tmpllk_indi_hetero = tf.exp(-0.5*tf.square(self.y - mean_stack)/(var_stack + 1e-5))/(2.0*np.pi*(var_stack + 1e-5))**0.5
+            
+        llk_hetero = tf.multiply(tmpllk_indi_hetero, self.gates) 
+        self.nllk_hetero = tf.reduce_sum(-1.0*tf.log(tf.reduce_sum(llk_hetero, axis = -1) + 1e-5))
+        
+        # lk_inv
+        # [B C]
+        tmpllk_indi_hetero_inv = tf.exp(-0.5*tf.square(self.y - mean_stack)*inv_var_stack)*(0.5/np.pi*inv_var_stack)**0.5
+            
+        llk_hetero_inv = tf.multiply(tmpllk_indi_hetero_inv, self.gates) 
+        self.nllk_hetero_inv = tf.reduce_sum(-1.0*tf.log(tf.reduce_sum(llk_hetero_inv, axis = -1) + 1e-5))
+        
+        # mse
+        # [B C]
+        tmpllk_indi_const = tf.exp(-0.5*tf.square(self.y - mean_stack))/(2.0*np.pi)**0.5
+            
+        llk_const = tf.multiply(tmpllk_indi_const, self.gates) 
+        self.nllk_const = tf.reduce_sum(-1.0*tf.log(tf.reduce_sum(llk_const, axis = -1) + 1e-5))
+        
+        # ---- prediction 
         
         if distr_type == 'gaussian':
             
             # component mean
             self.py_mean_indi = mean_stack
             
-            # component variance
-            self.py_var_indi = mean_var
-            
             # mixture mean
+            # [B]
             self.py = tf.reduce_sum(tf.multiply(mean_stack, self.gates), 1)
             
             # mixture variance
-            sq_mean_stack =  var_stack + tf.square(mean_stack)
-            mix_sq_mean = tf.reduce_sum(tf.multiply(sq_mean_stack, self.gates), 1)
+            if self.loss_type == 'lk':
+                
+                # component variance
+                self.py_var_indi = var_stack
+                
+                sq_mean_stack = var_stack + tf.square(mean_stack)
+                mix_sq_mean = tf.reduce_sum(tf.multiply(sq_mean_stack, self.gates), 1)
             
+            elif self.loss_type == 'lk_inv':
+                
+                # component variance
+                self.py_var_indi = inv_var_stack
+                
+                sq_mean_stack = 1.0/(inv_var_stack + 1e-5) + tf.square(mean_stack)
+                mix_sq_mean = tf.reduce_sum(tf.multiply(sq_mean_stack, self.gates), 1)
+                
+            elif self.loss_type == 'mse':
+                
+                # component variance
+                self.py_var_indi = 1.0
+                
+                sq_mean_stack = 1.0 + tf.square(mean_stack)
+                mix_sq_mean = tf.reduce_sum(tf.multiply(sq_mean_stack, self.gates), 1)
+                
+            # [B]
             self.py_var = mix_sq_mean - tf.square(self.py)
         
         else:
-            print '[ERROR] distribution type'
+            print('[ERROR] distribution type')
         
 
-#   initialize loss and optimization operations for training
+    #   initialize loss and optimization operations for training
     def train_ini(self):
+        
+        # loss, nllk, py_mean, py_std
         
         # loss
         if self.loss_type == 'mse':
@@ -283,69 +354,97 @@ class mixture_linear():
             self.mse = tf.reduce_mean(tf.square(self.y - self.py))
             
             self.loss = self.mse + self.regularization
+            self.nllk = self.nllk_const
             
-            self.nllk = 
+        elif self.loss_type == 'lk_inv':
             
-            self.py = 
-            self.py_std = 
+            self.loss = self.nllk_hetero_inv + self.regularization + self.l2*regu_var
+            self.nllk = self.nllk_hetero_inv
             
         elif self.loss_type == 'lk':
             
-            self.loss = self.nllk + self.regularization
+            self.loss = self.nllk_hetero + self.regularization + self.l2*regu_var
+            self.nllk = self.nllk_hetero
             
-            self.nllk = 
-            
-            self.py = 
-            self.py_std = 
-        
         else:
             print '[ERROR] loss type'
         
-        self.train = tf.train.AdamOptimizer(learning_rate = self.LEARNING_RATE)
+        # [B]
+        self.py_mean = self.py
+        self.py_std = tf.sqrt(self.py_var)
+        
+        self.train = tf.train.AdamOptimizer(learning_rate = self.lr)
         self.optimizer =  self.train.minimize(self.loss)
         
         self.init = tf.global_variables_initializer()
         self.sess.run(self.init)
         
-    #   training on batch of data
-    def train_batch(self, auto_train, x_train, y_train, keep_prob ):
         
-        # !
-        _, c = self.sess.run([self.optimizer, self.loss],\
-                             feed_dict={self.auto: auto_train, \
-                                        self.x:x_train, self.y:y_train, self.keep_prob:keep_prob })
+    #   training on batch of data
+    def train_batch(self, x_list, y, keep_prob ):
+        
+        data_dict = {}
+        for idx, i in enumerate(self.x_ph_list):
+            data_dict[i] = x_list[idx]
+        
+        data_dict[self.y] = y
+        data_dict[self.keep_prob] = keep_prob
+        
+        
+        _, c = self.sess.run([self.optimizer, self.loss],
+                             feed_dict = data_dict)
+                             
         return c
     
     
+    #   error metric
     def inference_ini(self):
         
-        # --- error metric
-        
         # RMSE
-        self.rmse = tf.sqrt(tf.losses.mean_squared_error(self.y, self.y_hat))
+        self.rmse = tf.sqrt(tf.losses.mean_squared_error(self.y, self.py))
+        
         # MAE
-        self.mae  = tf.reduce_mean(tf.abs(self.y - self.y_hat))
+        self.mae  = tf.reduce_mean(tf.abs(self.y - self.py))
         
         # MAPE
-        mask = tf.greater(tf.abs(self.y), 0.00001)
+        # based on groundtruth y
+        mask = tf.greater(tf.abs(self.y), 1e-5)
         
         y_mask = tf.boolean_mask(self.y, mask)
-        y_hat_mask = tf.boolean_mask(self.y_hat, mask)
+        y_hat_mask = tf.boolean_mask(self.py, mask)
         
-        self.mape = tf.reduce_mean(tf.abs((y_mask - y_hat_mask)/(y_mask+1e-10)))
-    
+        self.mape = tf.reduce_mean(tf.abs((y_mask - y_hat_mask)/(y_mask + 1e-10)))
+        
     
     #   infer givn testing data
-    def inference(self, auto_test, x_test, y_test, keep_prob):
+    def inference(self, x_list, y, keep_prob, bool_indi_eval):
         
-        return self.sess.run([self.rmse, self.mae, self.mape, self.nllk], 
-                             feed_dict = {self.auto:auto_test, \
-                                          self.x:x_test,  self.y:y_test, self.keep_prob:keep_prob })
+        # rmse, mae, mape, nllk, py_mean, py_std
+        
+        data_dict = {}
+        for idx, i in enumerate(self.x_ph_list):
+            data_dict[i] = x_list[idx]
+        
+        data_dict[self.y] = y
+        data_dict[self.keep_prob] = keep_prob 
+        
+        rmse, mae, mape, nllk = self.sess.run([self.rmse, self.mae, self.mape, self.nllk], feed_dict = data_dict)
+        
+        if bool_indi_eval == True:
+            
+            py_mean, py_std = self.sess.run([self.py_mean, self.py_std], feed_dict = data_dict)
+            
+        else:
+            py_mean = None
+            py_std = None
+            
+        return rmse, mae, mape, nllk, py_mean, py_std
+    
     
     # collect the optimized variable values
     def collect_coeff_values(self, vari_keyword):
-        return [ tf_var.name for tf_var in tf.trainable_variables() if (vari_keyword in tf_var.name) ],\
-    [ tf_var.eval() for tf_var in tf.trainable_variables() if (vari_keyword in tf_var.name) ]
+        return [tf_var.name for tf_var in tf.trainable_variables() if (vari_keyword in tf_var.name)],
+    [tf_var.eval() for tf_var in tf.trainable_variables() if (vari_keyword in tf_var.name)]
     
     
     # restore the model from the files
@@ -358,5 +457,3 @@ class mixture_linear():
         
         return
     
-    
-        
